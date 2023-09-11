1a451c058eab90187e646dbf8b165ba51c4c8fdb
Юлия Бровина <Tyuiu.BrovinaYV@outlook.com>
2023-09-11 20:02:49 +0500

    Task6

---
+++ Tyuiu.BrovinaYV.Sprint0.Task6.V0.Lib/DataService.cs
@@ -0,0 +1,48 @@
+﻿namespace Tyuiu.BrovinaYV.Sprint0.Task6.V0.Lib
+{
+    public class DataService
+    {
+        public static object AdditionArray (int[] numbers)
+        {
+            var total = 0;
+            for (var i =0; i<numbers.Length; i++)
+            {
+                total = total + numbers[i];
+            }
+            
+            return total;
+
+        }
+        //предусловие
+        public static object SubtractionArray(int[] numbers)
+        {
+            var total = 0;
+            int index = 0;
+
+            while (index<numbers.Length)
+            {
+                total = total - numbers[index];
+                index++;
+            }
+            return total;
+
+        }
+        //постусловие
+        public static object MultiplicationArray(int[] numbers)
+        {
+            var total = 1;
+            int index = 0;
+            
+                do
+                {
+                    total = total * numbers[index];
+                    index++;
+                }
+                while (index < numbers.Length);
+
+            return total;
+
+        }
+
+    }
+}
